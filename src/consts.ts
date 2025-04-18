e0ba4ac41e94d67fb40b3315f0527c19b7493d2c
searchsakib <searchsakib@gmail.com>
2025-04-18 07:49:02 +0600

    varioud issues fixed

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Codda's Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION =
+  "Welcome to Codda's Blog, where ideas from one brain transfers to another brain, it's called reading btw.";
